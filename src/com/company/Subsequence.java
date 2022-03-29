5d38714259c4daf01a498ff488345f00b2dff4c3
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-03-29 12:33:39 +0530

    Done with unique subsequence as well.

--- src/com/company/Subsequence.java
+++ src/com/company/Subsequence.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.HashSet;
+
 public class Subsequence {
     public static void FindSubsequence(String str, int idx, String newString) {
         if (idx == str.length()) {
@@ -7,17 +9,36 @@ public class Subsequence {
             return;
         }
         char currElement = str.charAt(idx);
+        FindSubsequence(str, idx + 1, newString + currElement);
+        FindSubsequence(str, idx + 1, newString);
+    }
+
+    public static void FindUniqueSubsequence(String str, int idx, String newString, HashSet<String> set) {
+        if (idx == str.length()) {
+            if (!set.contains(newString)) {
+                set.add(newString);
+                System.out.println(newString);
+            }
+            return;
+        }
+        char currElement = str.charAt(idx);
 
 //        giving an option to character to get added in subsequence or not
-        FindSubsequence(str, idx + 1, newString + currElement);
+        FindUniqueSubsequence(str, idx + 1, newString + currElement, set);
 //        not to be
-        FindSubsequence(str, idx + 1, newString);
+        FindUniqueSubsequence(str, idx + 1, newString, set);
 
     }
 
     public static void main(String[] args) {
         String str = "abc";
+        System.out.println("All subsequence");
         FindSubsequence(str, 0, "");
+        HashSet<String> set = new HashSet<>();
+        String str1 = "aaa";
+        System.out.println("unique subsequence");
+        FindUniqueSubsequence(str1, 0, "", set);
+
 
     }
 }
