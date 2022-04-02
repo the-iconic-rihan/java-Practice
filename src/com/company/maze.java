fc1cad0bfce252e4942a39f4b4587b0e6d5df29b
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-04-02 12:42:41 +0530

    finding the maximun path in maze.

---
+++ src/com/company/maze.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class maze {
+    public static int countPath(int i, int j, int n, int m) {
+
+        if (i == n || j == m) {
+            return 0;
+        }
+//        if we are at the corner cells just move to one direction
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        }
+//        move downwards
+        int downPaths = countPath(i + 1, j, n, m);
+//        move right
+        int rightPath = countPath(i, j + 1, n, m);
+        return downPaths + rightPath;
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+        System.out.println(countPath(0, 0, n, m));
+    }
+}
