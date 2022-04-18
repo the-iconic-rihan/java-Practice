6f4595b1c63c3242fc5a0220e7905b3acc5f2b1c
Rihan Bagwan <rihanbagwan5017@gmail.com>
2022-04-18 11:49:53 +0530

    Reformated the code of permutation

--- src/com/company/permutation.java
+++ src/com/company/permutation.java
@@ -9,7 +9,9 @@ public class permutation {
 //       loop to fill the 1st chair by first char.
         for (int i = 0; i < str.length(); i++) {
             char currElement = str.charAt(i);
-//            if you choose 'a' as currElement then "abc" => "bc" , if you choose 'b' as currElement then "abc" => "ac",if you choose 'c' as currElement then "abc" => "ab".
+/*          if you choose 'a' as currElement then "abc" => "bc" ,
+if you choose 'b' as currElement then "abc" => "ac",if you choose 'c' as currElement then "abc" => "ab".
+ */
             String newStr = str.substring(0, i) + str.substring(i + 1);
             printPermutation(newStr, permutation + currElement);
         }
